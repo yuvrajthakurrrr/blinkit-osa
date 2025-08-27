3df21a6f734ece85690d8d83b4aebc2bba3d142e
Yuvraj Singh <vikramsingh5682@gmail.com>
2025-08-27 15:56:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,19 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name="blinkitpdp",   # Replace with your project name
-    version="0.1.0",
-    packages=find_packages(),
-    install_requires=[
-        "pillow==9.1.1",
-        "lxml",
-        "selenium",
-        "pyodbc",
-        "openpyxl",
-        "requests",
-        "beautifulsoup4",
-        "azure-storage-blob",
-        "SQLAlchemy",
-        "boto3",
-        "amazoncaptcha",
-        "undetected-chromedriver",
-        "distutils-pytest",
-        "pandas",
-        "chromedriver-autoinstaller",
-        "requests_auth_aws_sigv4",
-        "cloudscraper",
-        "flair==0.14.0",
-        "curl-cffi",
-        "xlrd",
-        "setuptools"  # No need for --upgrade here, just specify
-    ],
-    description="Blinkit OSA web scraper",
-    url="https://github.com/yuvrajthakurrrr/blinkit-osa",
-    python_requires=">=3.8",  # adjust based on your Python version
+setuptools.setup(
+    name='BlinkitOSA',
+    version='0.0.0.033',
+    author='Yuvraj Singh',
+    author_email='vikramsingh5682@gmail.com',
+    description='Blinkit OSA package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/yuvrajthakurrrr/blinkit-osa',
+    project_urls = {
+        "Bug Tracker": "https://github.com/mike-huls/toolbox/issues"
+    },
+    license='MIT',
+    packages=['BlinkitOSA'],
+    install_requires=['curl-cffi','requests','sqlalchemy','boto3','requests_auth_aws_sigv4','pandas','python-dateutil']
 )
+
